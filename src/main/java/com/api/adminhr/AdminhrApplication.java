2170b47c2f92a49eda722979e3c03a77c138aadb
Gustafa Bayu <faafa13@gmail.com>
2023-06-23 14:07:16 +0700

    first commit

---
+++ src/main/java/com/api/adminhr/AdminhrApplication.java
@@ -0,0 +1,13 @@
+package com.api.adminhr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminhrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminhrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/adminhr/AdminhrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.adminhr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminhrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
